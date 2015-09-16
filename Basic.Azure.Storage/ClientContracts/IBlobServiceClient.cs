d5c250b0b9fe16fe478e252e21fcba6b599d9389
Brian McBrayer <bmcbrayer@precisionlender.com>
2015-09-16 16:38:51 -0400

    Removed unused using

--- Basic.Azure.Storage/ClientContracts/IBlobServiceClient.cs
+++ Basic.Azure.Storage/ClientContracts/IBlobServiceClient.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Basic.Azure.Storage.Communications.BlobService;
 using Basic.Azure.Storage.Communications.BlobService.BlobOperations;
